14f430060fff73d6d0d79c17ba69e40a75ad0ca5
george-chatzopoulos <georgechatzopoulos2805@gmai.com>
2025-03-26 01:06:36 +0200

    Add a smart constructor for Rank to validate the numberic value (2 <= n <= 10)

--- blackjack.hs
+++ blackjack.hs
@@ -2,13 +2,20 @@
 data Suit = Hearts | Spades | Diamonds | Clubs
   deriving (Show, Eq)
 
+
 data Rank = Numeric Int | Jack | Queen | King | Ace
   deriving (Show, Eq)
+
+mkNumberic :: Int -> Maybe Rank
+mkNumberic n | n >= 2 && n <= 10 = Just (Numeric n)
+             | otherwise         = Nothing
 
 data Card = Card { rank :: Rank, suit :: Suit }
   deriving (Show, Eq)
 
+
 type Hand = [Card]
+
 
 data Player = Bank | Guest
   deriving (Show, Eq)
