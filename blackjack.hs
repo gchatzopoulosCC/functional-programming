9dbc7a39a505f89f2b65bd53ad6c7ee5186e98c3
george-chatzopoulos <georgechatzopoulos2805@gmai.com>
2025-03-26 01:39:26 +0200

    Create the faceCards function

--- blackjack.hs
+++ blackjack.hs
@@ -2,7 +2,7 @@
 data Suit = Hearts | Spades | Diamonds | Clubs
   deriving (Show, Eq)
 
-
+-- Rank includes the mkNumberic Smart Constructor
 data Rank = Numeric Int | Jack | Queen | King | Ace
   deriving (Show, Eq)
 
@@ -13,10 +13,15 @@ mkNumberic n | n >= 2 && n <= 10 = Just (Numeric n)
 data Card = Card { rank :: Rank, suit :: Suit }
   deriving (Show, Eq)
 
-
 type Hand = [Card]
-
 
 data Player = Bank | Guest
   deriving (Show, Eq)
+
+
+-- Functionality (Chatzopoulos)
+faceCards :: Hand -> Integer
+faceCards [] = 0
+faceCards (x:xs) | rank x `elem` [Jack, Queen, King] = 1 + faceCards xs
+                 | otherwise                         = faceCards xs
 
