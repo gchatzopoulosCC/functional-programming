c6dec8e5d8a2a4a7fa81de3cac2674032b01e272
lkostoski <lkostoski@york.citycollege.eu>
2025-03-26 17:39:08 +0200

    fix employeesByTenure

--- EmployeeQueries.hs
+++ EmployeeQueries.hs
@@ -1,5 +1,4 @@
-import Data.List (sort, group, maximumBy)
-import Data.Maybe (fromJust)
+import Data.List (sortOn, groupBy, maximumBy)
 
 type EId = String
 
@@ -18,8 +17,8 @@ data Employee = Emp
 data Date = Date 
   { 
     year  :: Int,
-    month :: Int,
-    day   :: Int
+    month :: Int, --month should not exceed 12
+    day   :: Int  --no invalid days
   }
   deriving (Show, Eq, Ord)
 
@@ -34,6 +33,56 @@ employeesWithOverlappingPermits emps = [(empId emp1, empId emp2) |
   Just permit2 <- [permit emp2],
   joinedOn emp1 <= expiryDate permit2 && expiryDate permit1 >= joinedOn emp2
   ]
+
+
+--2. ehh
+employeesByTenure :: [Employee] -> [(Int, [Employee])]
+employeesByTenure emps = map formatGroup (groupByTenure (sortByTenure (assignTenure emps)))
+
+assignTenure :: [Employee] -> [(Int, Employee)]
+assignTenure emps = [(calculateTenure emp, emp) | emp <- emps]
+
+sortByTenure :: [(Int, Employee)] -> [(Int, Employee)]
+sortByTenure = sortOn fst
+
+groupByTenure :: [(Int, Employee)] -> [[(Int, Employee)]]
+groupByTenure = groupBy (\(t1, _) (t2, _) -> t1 == t2)
+
+formatGroup :: [(Int, Employee)] -> (Int, [Employee])
+formatGroup group = (fst (head group), map snd group)
+
+currDate :: Date
+currDate = Date 2025 3 22
+
+yearDiff :: Date -> Date -> Int
+yearDiff date1 date2 = year date2 - year date1
+
+calculateTenure :: Employee -> Int
+calculateTenure emp =
+  let endDate = case leftOn emp of
+                    Just date -> date
+                    Nothing   -> currDate
+  in yearDiff (joinedOn emp) endDate
+
+--3.
+longestWorkingEmployee :: [Employee] -> Maybe Employee
+longestWorkingEmployee []   = Nothing
+longestWorkingEmployee emps = Just (maximumBy (\emp1 emp2 -> compare (calculateTenure emp1) (calculateTenure emp2)) emps)
+
+--4. confident
+withExpiredPermit :: [Employee] -> Date -> [EId]
+withExpiredPermit emps currDate = [empId emp | emp <- emps, Just p <- [permit emp], currDate > expiryDate p]
+
+--5. confident
+avgYearsWorked :: [Employee] -> Double
+avgYearsWorked [] = 0
+avgYearsWorked emps = if totalEmployees == 0
+                      then 0
+                      else tenures emps / totalEmployees
+  where
+   totalEmployees = fromIntegral $ length emps
+   tenures []    = 0
+   tenures emps  = fromIntegral $ sum [calculateTenure emp | emp <- emps]
 
 testEmployees :: [Employee]
 testEmployees = 
@@ -80,41 +129,3 @@ testEmployees =
         , leftOn = Just (Date 2022 11 30)
         }
   ]
-
-
---2. nop
-employeesByTenure :: [Employee] -> [(Int, [Employee])]
-employeesByTenure emps = [(calculateTenure emp, [emp]) | emp <- emps]
-
-currDate :: Date
-currDate = Date 2025 3 22
-
-yearDiff :: Date -> Date -> Int
-yearDiff date1 date2 = year date2 - year date1
-
-calculateTenure :: Employee -> Int
-calculateTenure emp =
-  let endDate = case leftOn emp of
-                    Just date -> date
-                    Nothing   -> currDate
-  in yearDiff (joinedOn emp) endDate
-
---3.
-longestWorkingEmployee :: [Employee] -> Maybe Employee
-longestWorkingEmployee []   = Nothing
-longestWorkingEmployee emps = Just (maximumBy (\emp1 emp2 -> compare (calculateTenure emp1) (calculateTenure emp2)) emps)
-
---4. confident
-withExpiredPermit :: [Employee] -> Date -> [EId]
-withExpiredPermit emps currDate = [empId emp | emp <- emps, Just p <- [permit emp], currDate > expiryDate p]
-
---5. confident
-avgYearsWorked :: [Employee] -> Double
-avgYearsWorked [] = 0
-avgYearsWorked emps = if totalEmployees == 0
-                      then 0
-                      else tenures emps / totalEmployees
-  where
-   totalEmployees = fromIntegral $ length emps
-   tenures []    = 0
-   tenures emps  = fromIntegral $ sum [calculateTenure emp | emp <- emps]
