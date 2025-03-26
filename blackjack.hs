7f3c5c2a8fb3acb8215a1c4925b7068e642b1d32
george-chatzopoulos <georgechatzopoulos2805@gmai.com>
2025-03-26 13:04:40 +0200

    Make a smart constructor for non-Numeric ranks

--- blackjack.hs
+++ blackjack.hs
@@ -5,7 +5,7 @@ data Suit = Hearts | Spades | Diamonds | Clubs
 data Rank = Numeric Integer | Jack | Queen | King | Ace
   deriving (Show, Eq, Ord)
 
-data Card = Card { rank :: Rank, suit :: Suit }
+data Card = Card { rank :: Maybe Rank, suit :: Suit }
   deriving (Show, Eq, Ord)
 
 type Hand = [Card]
@@ -14,7 +14,11 @@ data Player = Bank | Guest
   deriving (Show, Eq, Ord)
 
 
--- Smart constructor for Rank
-mkNumberic :: Integer -> Maybe Rank
-mkNumberic n | n >= 2 && n <= 10 = Just (Numeric n)
-             | otherwise         = Nothing
+-- Smart constructors for Rank
+mkNumeric :: Integer -> Maybe Rank
+mkNumeric n | n >= 2 && n <= 10 = Just (Numeric n)
+            | otherwise         = Nothing
+
+mkNonNumeric :: Rank -> Maybe Rank
+mkNonNumeric rank = Just rank
+
