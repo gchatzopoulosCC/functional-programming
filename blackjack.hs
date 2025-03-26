20897b20f7778d7b896b4b592e7af0c33da15c6e
george-chatzopoulos <georgechatzopoulos2805@gmai.com>
2025-03-26 12:38:42 +0200

    Reorganize the file

--- blackjack.hs
+++ blackjack.hs
@@ -1,22 +1,20 @@
 -- Types
 data Suit = Hearts | Spades | Diamonds | Clubs
-  deriving (Show, Eq)
+  deriving (Show, Eq, Ord)
 
-
-data Rank = Numeric Int | Jack | Queen | King | Ace
-  deriving (Show, Eq)
-
-mkNumberic :: Int -> Maybe Rank
-mkNumberic n | n >= 2 && n <= 10 = Just (Numeric n)
-             | otherwise         = Nothing
+data Rank = Numeric Integer | Jack | Queen | King | Ace
+  deriving (Show, Eq, Ord)
 
 data Card = Card { rank :: Rank, suit :: Suit }
-  deriving (Show, Eq)
-
+  deriving (Show, Eq, Ord)
 
 type Hand = [Card]
 
-
 data Player = Bank | Guest
-  deriving (Show, Eq)
+  deriving (Show, Eq, Ord)
 
+
+-- Smart constructor for Rank
+mkNumberic :: Integer -> Maybe Rank
+mkNumberic n | n >= 2 && n <= 10 = Just (Numeric n)
+             | otherwise         = Nothing
